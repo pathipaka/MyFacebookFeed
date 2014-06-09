6a05535d5703f572f660cb7347159c7d9eab5067
Om Pathipaka <pathipaka@gmail.com>
2014-06-09 01:26:27 -0700

    Adding the Xcode files

---
+++ MyFacebookFeed/NotificationsViewController.m
@@ -0,0 +1,39 @@
+//
+//  NotificationsViewController.m
+//  MyFacebookFeed
+//
+//  Created by Om Pathipaka on 6/8/14.
+//  Copyright (c) 2014 Om Pathipaka. All rights reserved.
+//
+
+#import "NotificationsViewController.h"
+
+@interface NotificationsViewController ()
+
+@end
+
+@implementation NotificationsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+     self.navigationItem.title = @"Notifications";
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
